d9aad96c47877e7c1bde67d83715a2ac32715fc4
Jan Johannes <lucidNTR@users.noreply.github.com>
2021-06-09 22:46:48 +0200

    reproduction test for bug deployctl/issues/49

--- examples/hello.js
+++ examples/hello.js
@@ -1,11 +1,17 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(
-    new Response("Hello world!", {
-      status: 200,
-      headers: {
-        server: "denosr",
-        "content-type": "text/plain",
-      },
-    }),
-  );
+// addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     new Response("Hello world!", {
+//       status: 200,
+//       headers: {
+//         server: "denosr",
+//         "content-type": "text/plain",
+//       },
+//     }),
+//   );
+// });
+
+
+self.addEventListener("fetch", (event) => {
+  console.log(event)
+  event.respondWith(fetch('http://google.com'))
 });
